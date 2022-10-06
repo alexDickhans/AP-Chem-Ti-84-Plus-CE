c9a80ff326fdacf0c6dae08f4db8907d8a1e65f8
Alex Dickhans <55064247+alexDickhans@users.noreply.github.com>
2022-10-05 21:23:48 -0600

    Finish formatting

--- CHEM.py
+++ CHEM.py
@@ -1,8 +1,55 @@
 
-cation1list = {"Lithium": "Li⁺")}
+cation1list = {"Lithium": "Li^+"}
+cation2list = {"Lithium": "Li^+"}
+cation3list = {"Lithium": "Li^+"}
+cation4list = {"Lithium": "Li^+"}
+cation5list = {"Lithium": "Li^+"}
+
+anion1list = {"": ""}
+anion2list = {"": ""}
+anion3list = {"": ""}
+anion4list = {"": ""}
+anion5list = {"": ""}
+anion6list = {"": ""}
 
 def cation1():
     for key in cation1list:
         print("{}: {}".format(key, cation1list[key]))
 
-cation1()
+def cation1():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+def cation1():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+def cation1():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+def cation1():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+
+
+def anion1():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+def anion2():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+def anion3():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+def anion4():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
+
+def anion5():
+    for key in cation1list:
+        print("{}: {}".format(key, cation1list[key]))
